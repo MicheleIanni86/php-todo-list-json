36929e11a36b49b459b5fa281dcc7956902e5873
MicheleIanni86 <micheleianni86@gmail.com>
2024-02-29 18:27:59 +0100

    feat: Add Main.js

---
+++ frontend/main.js
@@ -0,0 +1,53 @@
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+
+            title: "ciaoooo...funziono!!!"
+        };
+    },
+});
+
+
+app.mount('#root');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// schede: [
+//     {
+//         nameScheda: 'Prendere Latte',
+//         doneScheda: true,
+//     },
+//     {
+//         nameScheda: 'Pagare Mutuo',
+//         doneScheda: false,
+//     },
+//     {
+//         nameScheda: 'Impastare Pizza',
+//         doneScheda: false,
+//     },
+//     {
+//         nameScheda: 'Prove con Band',
+//         doneScheda: true,
+//     },
+//     {
+//         nameScheda: 'Fare Tagliando Moto',
+//         doneScheda: true,
+//     },
+//     {
+//         nameScheda: 'Seguire Lezione Vue',
+//         doneScheda: false,
+//     },
+
+// ],
